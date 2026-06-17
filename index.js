a58a0f38a5369092d231ac3102df27a5f14b688d
Fer Luna @ ISTEA && Educación IT <128251567+FernandoIstea@users.noreply.github.com>
2026-06-17 20:55:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,23 +22,23 @@ const API = 'api/v1'
 
 app.use(express.json())
 // --- Routes ---
-app.get(`${API}/`, (req, res)=> res.redirect('/hello-world') )
-app.get(`${API}/hello-world`, helloWorld)
+app.get(`/`, (req, res)=> res.redirect('/hello-world') )
+app.get(`/hello-world`, helloWorld)
 // ADMIN
-app.post(`${API}/create-admin-profile`, createAdminProfile)
-app.get(`${API}/recover-profile`, recoverProfile)
+app.post(`/create-admin-profile`, createAdminProfile)
+app.get(`/recover-profile`, recoverProfile)
 // CATEGORIES
-app.get(`${API}/categories`, getCategories)
-app.post(`${API}/categories`, createCategory)
+app.get(`/categories`, getCategories)
+app.post(`/categories`, createCategory)
 // PRODUCTS
-app.get(`${API}/products`, getProducts)
-app.get(`${API}/products/:id`, getProductById)
-app.get(`${API}/products/name/:name`, filterProductsByName)
-app.post(`${API}/products`, createProduct)
-app.put(`${API}/products/:id`, updateProduct)
-app.delete(`${API}/products/:id`, softDeleteProduct)
+app.get(`/products`, getProducts)
+app.get(`/products/:id`, getProductById)
+app.get(`/products/name/:name`, filterProductsByName)
+app.post(`/products`, createProduct)
+app.put(`/products/:id`, updateProduct)
+app.delete(`/products/:id`, softDeleteProduct)
 
-app.use((req, res) =>   res.status(404).json({ errorMessage: `Endpoint '${API}${req.url}' no encontrado.` }) )
+app.use((req, res) =>   res.status(404).json({ errorMessage: `Endpoint '${req.url}' no encontrado.` }) )
 
 // --- Server Start ---
 app.listen(PORT, () => {
