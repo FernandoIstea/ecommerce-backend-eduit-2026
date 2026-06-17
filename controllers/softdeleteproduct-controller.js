aeaea85722337180331aaa86b7edf48b60ae2092
Fer Luna @ ISTEA && Educación IT <128251567+FernandoIstea@users.noreply.github.com>
2026-06-17 14:44:12 -0300

    Update softdeleteproduct-controller.js

--- controllers/softdeleteproduct-controller.js
+++ controllers/softdeleteproduct-controller.js
@@ -7,7 +7,7 @@ import { MdbClass } from '../services/MdbClass.js'
  * @description Apply a soft delete in an existing product by ID.
  */
 export const softDeleteProduct = async (req, res) => {
-    let uid
+    let uid, status = 400
     const ecommerceApiKey = req.headers['ecommerceapikey']
     const id = req.params.id
 
@@ -27,21 +27,25 @@ export const softDeleteProduct = async (req, res) => {
 
     try {
         if (result.valid) {
+            uid = result.data.userId
             await MdbClass.connect(process.env.MONGO_DB_CONNECTION)
-            const deletedProduct = await MdbClass.softDeleteProduct(id, result.data.userId)
+            const deletedProduct = await MdbClass.softDeleteProduct(id, uid)
 
             if (deletedProduct.status === "Ok") {
-                return res.status(204).json({ success: true,
-                                              data: {}
-                                            })
+                return res.status(204).json({ success: true, data: {} })
+            } else if (deletedProduct.errorMessage.includes('Product not found')) {
+                status = 404
+                throw new Error(`Product with id: '${id}' was not found.`)
             } else {
+                status = 500
                 throw new Error(`Error trying to delete the product with ID: ${id}`)
             }
         } else {
+            status = 401
             throw new Error('You must inform a valid token to continue.')
         }
     } catch (error) {
-        return res.status(500).json({
+        return res.status(status).json({
             success: false,
             errorMessage: `Error deleting product: ${error.message}`
         })
